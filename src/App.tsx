8ad6d028f793df2876a1632f47d4d8a34ae694bb
Arthur <goolebyaqa@gmail.com>
2024-04-23 00:53:13 +0500

    feat: add loading while user waits

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,24 @@ import Header from './components/Header'
 import OrdersStage from './components/OrdersStage'
 import { IOrder } from './Types/Types'
 import SearchSection from './components/SerachSection'
+import {motion} from 'framer-motion';
 
 function App() {
-  const [orders, setOrders] = useState<[] | IOrder[]>([])
+  const [orders, setOrders] = useState<[] | IOrder[]>([]);
+  const [error, setError] = useState('');
+  const [fetching, setFetching] = useState(false);
 
   useEffect(() => {
     async function fetchOrders() {
       try {
+      setFetching(true);
       const response = await fetch("https://udevs-db-default-rtdb.asia-southeast1.firebasedatabase.app/orders.json")
       const orders = await response.json();
       setOrders(orders);
       } catch (err) {
-        throw new Error('Failed to fetch order from db');
+        setError('Could not fetch data from firebase');
       }
+      setFetching(false);
     }
     fetchOrders();
   }, [])
@@ -25,7 +30,10 @@ function App() {
     <>
       <Header userLetter={'D'}/> 
       <SearchSection />
-      <OrdersStage orders={orders}/>
+      {error && <p>{error}</p>}
+      {fetching ? 
+      <motion.div initial={{scale: 2}} animate={{scale: 1}} transition={{repeat: Infinity, duration: 4, repeatDelay: 0, ease: "linear", bounce: 0}}>Loading...</motion.div>:
+      <OrdersStage orders={orders}/>}
     </>
   )
 }
